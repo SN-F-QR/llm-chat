86fab6cee4ae241dabf64b897e63e405fb88b5f1
SN-F-QR <43869889+SN-F-QR@users.noreply.github.com>
2025-04-29 22:56:28 +0900

    fix: navbar style

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 const NavBar = () => {
   return (
-    <nav>
-      <div className="sticky top-0 flex h-12 w-full items-center justify-between border-b border-gray-200 bg-white p-4 shadow-sm"></div>
+    <nav className="sticky top-0">
+      <div className="flex h-12 w-full items-center justify-between border-b border-gray-200 bg-white p-4 shadow-sm"></div>
     </nav>
   );
 };
