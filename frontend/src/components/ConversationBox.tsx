c94f151e6d93b98206079f1ee35d5d0d39170d47
SN-F-QR <43869889+SN-F-QR@users.noreply.github.com>
2025-05-06 20:27:46 +0900

    ✨ Feat: style code block
    change to md-to-jsx package, using hljs cdn

--- frontend/src/components/ConversationBox.tsx
+++ frontend/src/components/ConversationBox.tsx
@@ -1,7 +1,7 @@
 import { Message } from '../types/types';
 import { Sparkle, RotateCcw, TriangleAlert, CircleStop } from 'lucide-react';
-import Markdown from 'react-markdown';
-import { useRef } from 'react';
+import Markdown from 'markdown-to-jsx';
+import { useRef, useEffect } from 'react';
 
 /**
  * A box showing the conversation between the user and the assistant
@@ -62,7 +62,27 @@ const ConversationBox: React.FC<{
   );
 };
 
-const MessageBuble: React.FC<{ message: Message; updating: boolean }> = ({ message }) => {
+const MessageBuble: React.FC<{ message: Message; updating: boolean }> = ({ message, updating }) => {
+  const SyntaxHighlightedCode = (props: React.HTMLAttributes<HTMLElement>) => {
+    const ref = useRef<HTMLElement | null>(null);
+
+    useEffect(() => {
+      if (ref.current && props.className?.includes('lang-') && window.hljs && !updating) {
+        // use auto detection and avoid directly using highlightElement
+        const highlighted: { value: string } = window.hljs.highlightAuto(
+          ref.current.innerText ?? ''
+        );
+        ref.current.innerHTML = highlighted.value;
+
+        // hljs won't reprocess the element unless this attribute is removed
+        ref.current.removeAttribute('data-highlighted');
+      }
+    }, [props.className, props.children]);
+
+    // add hljs class to change the background color
+    return <code {...props} className={`${props.className} hljs rounded-2xl text-sm`} ref={ref} />;
+  };
+
   return (
     <div className="flex w-full">
       {message.role === 'user' ? (
@@ -75,7 +95,9 @@ const MessageBuble: React.FC<{ message: Message; updating: boolean }> = ({ messa
             <Sparkle className="size-6 text-purple-600" />
           </div>
           <div className="w-full p-2 break-words">
-            <Markdown>{message.content}</Markdown>
+            <Markdown options={{ overrides: { code: SyntaxHighlightedCode } }}>
+              {message.content}
+            </Markdown>
           </div>
         </div>
       )}
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+declare global {
+  interface Window {
+    hljs?: {
+      highlightElement: (element: HTMLElement) => void;
+      highlightAuto: (text: string) => { value: string };
+    };
+  }
+}
+
+export {};
