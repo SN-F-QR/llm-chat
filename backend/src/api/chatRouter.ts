4063daf67b5fc0ab16b15a81912be934d3cf9abb
SN-F-QR <43869889+SN-F-QR@users.noreply.github.com>
2025-05-23 23:55:22 +0900

    ✨ Feat: get chat messages API
    fix create chat api

--- backend/src/api/chatRouter.ts
+++ backend/src/api/chatRouter.ts
@@ -1,11 +1,14 @@
 import { Hono } from 'hono';
 import db from '../database';
-import { chatTable } from '../drizzle/schema';
+import { eq, and } from 'drizzle-orm';
+import { chatTable, messageTable, Role } from '../drizzle/schema';
 import { authMiddleware } from './authRouter';
 import { nanoid } from 'nanoid';
 
 import { GoogleGenAI } from '@google/genai';
 import { googleApiKey } from '../config';
+
+import NotFoundError from '../error/NotFoundError';
 
 const chatRouter = new Hono();
 const llm = new GoogleGenAI({ apiKey: googleApiKey });
@@ -28,6 +31,12 @@ const generateTitle = async (content: string) => {
   }
 };
 
+/**
+ * Creates a new chat list
+ * @route POST /chat
+ * @param {string} content of first chat message
+ * @return a chat info object
+ */
 chatRouter.post('/', authMiddleware, async (c) => {
   const { id: userId } = c.get('user');
   const { content }: { content: string } = await c.req.json();
@@ -45,10 +54,48 @@ chatRouter.post('/', authMiddleware, async (c) => {
       ownerId: userId,
     })
     .returning();
+
+  // Insert the first message
+  await db.insert(messageTable).values({
+    chatId: newChat[0].id,
+    content,
+    role: Role.user,
+  });
+
   c.status(201);
   return c.json({
     ...newChat[0],
     id: undefined,
+  });
+});
+
+/**
+ * Get messages of a chat
+ * @route GET /chat/:publicid
+ * @param {string} publicid - The public ID of the chat
+ * @return {object} - The chat info and messages
+ */
+chatRouter.get('/:publicid', authMiddleware, async (c) => {
+  const publicId = c.req.param('publicid');
+  const chat = await db.query.chatTable.findFirst({
+    where: and(eq(chatTable.publicId, publicId), eq(chatTable.ownerId, c.get('user').id)),
+    with: {
+      messages: true,
+    },
+  });
+  if (!chat) {
+    throw new NotFoundError('Chat not exist');
+  }
+  const { messages, ...chatInfo } = chat;
+  const returnMessages = messages.map((message) => {
+    const { id, chatId, ...messageInfo } = message;
+    return messageInfo;
+  });
+  c.status(200);
+  return c.json({
+    ...chatInfo,
+    id: undefined,
+    messages: returnMessages,
   });
 });
 
